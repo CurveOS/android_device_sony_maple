f5df81d5da12ab33b3b709f0aae53169bb934a19
Curve OS <lovaren@gmail.com>
2018-09-25 01:49:26 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineAgeOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,28 @@
 # limitations under the License.
 #
 
+# WARNING: Everything listed here will be built on ALL platforms,
+# including x86, the emulator, and the SDK.  Modules must be uniquely
+# named (liblights.tuna), and must build everywhere, or limit themselves
+# to only building on ARM if they include assembly. Individual makefiles
+# are responsible for having their own logic, for fine-grained control.
+
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter maple, $(TARGET_DEVICE)),)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),maple)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+MODEM_SYMLINKS := $(TARGET_OUT)/etc/customization/modem
+$(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Create modem links: $@"
+	@mkdir -p $@
+	$(hide) ln -sf amss_fsg_maple_tar.mbn $@/default
+	$(hide) ln -sf amss_fs_empty.mbn $@/reset_modemst1
+	$(hide) ln -sf amss_fs_empty.mbn $@/reset_modemst2
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(MODEM_SYMLINKS)
+
 endif
